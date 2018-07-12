66c497634a7928d5998bff01fb69a2da8bd2675e
Bhavik Kumar <bhavik@bravenewcoin.com>
2018-07-12 13:12:47 +1200

    Update based on microservice deployments

--- src/test/java/com/bnc/sbjb/rest/ControllerTest.java
+++ src/test/java/com/bnc/sbjb/rest/ControllerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -14,12 +15,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 public class ControllerTest {
 
+    @Value("${resource.path}")
+    private String basePath;
+
     @Autowired
     private TestRestTemplate template;
 
     @Test
     void testHelloWorld() {
-        String actual = template.getForObject("/hello", String.class);
+        String actual = template.getForObject(basePath + "/hello", String.class);
         assertThat(actual).isEqualTo("Hello World");
     }
 }
